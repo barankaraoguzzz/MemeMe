ab146b875cf645453cec6f3a3aab16193f7b3f6e
BARAN BATUHAN KARAOGUZ <baran.karaoguz@ogr.sakarya.edu.tr>
2021-02-18 14:30:59 +0300

    Removed scene delegate & created appcontainer

---
+++ MemeMe/AppContainers/AppContainer.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  MemeMe
+//
+//  Created by BARAN BATUHAN KARAOGUZ on 18.02.2021.
+//
+
+import Foundation
+
+let appContainer = AppContainer()
+
+class AppContainer {
+    
+}
