64ed3a7ff5eb86ef570e4e49867af6b04f24fa4f
BARAN BATUHAN KARAOGUZ <baran.karaoguz@ogr.sakarya.edu.tr>
2021-02-26 12:42:37 +0300

    Added action methods

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    ///IBOutlet veriables
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelTop: UITextField!
+    @IBOutlet weak var labelBottom: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
-    @IBAction func rightButton(_ sender: Any) {
+    
+    ///IBAction
+    
+    @IBAction func openCameraAct(_ sender: Any) {
         appContainer.imagePicker.showImagePicker(fromViewController: self, sourceType: .camera) { (image) in
-            print(image)
+            self.imageView.image = image
         }
     }
-    @IBAction func leftButton(_ sender: Any) {
+    
+    @IBAction func openGaleryAct(_ sender: Any) {
         appContainer.imagePicker.showImagePicker(fromViewController: self, sourceType: .photoLibrary) { (image) in
-            print(image)
+            self.imageView.image = image
         }
     }
+    
+    @IBAction func cancelButtonAct(_ sender: Any) {
+        
+    }
+    
+    @IBAction func shareButtonAct(_ sender: Any) {
+        
+    }
+    
 }
 
